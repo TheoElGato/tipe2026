c275801453d475f91b784aa8d7dc54bbdbadcfd9
elydev <elycraft.yt@gmail.com>
2025-10-06 18:01:06 +0200

    Working on save

--- include/save.h
+++ include/save.h
@@ -2,6 +2,21 @@
 #define SAVE_H
 
 #include <nlohmann/json.hpp>
+#include <fstream>
+#include <unistd.h>
+#include <limits.h>
+
+
+class SimDataSave {
+public:
+    SimDataSave();
+
+
+   
+private:
+	nlohmann::json data;
+
+};
 
 
 #endif // SAVE_H
--- src/save.cpp
+++ src/save.cpp
@@ -1,2 +1,29 @@
 #include "save.h"
 
+SimDataSave::SimDataSave() {
+
+	char hostname[HOST_NAME_MAX];
+	gethostname(hostname, HOST_NAME_MAX);
+	
+	
+
+	std::string name = "";
+	std::string host_name = hostname;
+	int generation = 0;
+	int simu_time = 0;
+	int evolution = 0;
+	int brains_number = 0;
+	int agents_number = 0;
+	int train_sessions = 0;
+	
+	data = {
+        {"name", "name"},
+        {"host_name", host_name},
+        {"generation", generation},
+        {"simu_time", simu_time},
+        {"evolution", evolution},
+        {"brains-number", brains_number},
+        {"agents-number", agents_number},
+        {"train_sessions",train_sessions}
+    };
+}
