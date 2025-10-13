8c804354db5c91b9a050934277dadbb983f7b146
TheoElGato <antoine.spadone@gmail.com>
2025-10-13 17:24:08 +0200

    correction

--- src/reproduction.cpp
+++ src/reproduction.cpp
@@ -1,11 +1,11 @@
 #include "reproduction.h"
 
 
-void reproduce(std::vector<Brain>* agents, std::vector<float> score_agent, int NB_BRAIN, float EVOLUTION, int bestKeep, int selectionPol){
+void reproduce(std::vector<Brain>* brain_agents, std::vector<float> score_agent, int NB_BRAIN, float EVOLUTION, int bestKeep, int selectionPol){
 
     // Fool proof
-    if ((*agents).size() != NB_BRAIN || score_agent.size() != NB_BRAIN) {
-        std::cerr << "Error: agents and score_agent vectors must have size NB_BRAIN." << std::endl;
+    if ((*brain_agents).size() != NB_BRAIN || score_agent.size() != NB_BRAIN) {
+        std::cerr << "Error: brain_agents and score_agent vectors must have size NB_BRAIN." << std::endl;
         exit(EXIT_FAILURE);
     }
     if (bestKeep > 100 || selectionPol > 100 || bestKeep < 0 || selectionPol < 0) {
@@ -17,12 +17,12 @@ void reproduce(std::vector<Brain>* agents, std::vector<float> score_agent, int N
     // Creation of the new brain vector
     std::vector<Brain> new_agents;
 
-    // Calculate number of best and worst agents to keep/kill
+    // Calculate number of best and worst brain_agents to keep/kill
     int num_best_to_keep = (bestKeep * NB_BRAIN) / 100;
     int num_selctionable = (selectionPol * NB_BRAIN) / 100;
 
-    // Sort agents by score
-    reverse_sorting_brain(agents, &score_agent, 0, NB_BRAIN - 1);
+    // Sort brain_agents by score
+    reverse_sorting_brain(brain_agents, &score_agent, 0, NB_BRAIN - 1);
 
 
     // Normalise score
@@ -45,13 +45,13 @@ void reproduce(std::vector<Brain>* agents, std::vector<float> score_agent, int N
             temp += score_agent[j];
         }
 
-        new_agents.emplace_back((*agents)[j]);
+        new_agents.emplace_back((*brain_agents)[j]);
         new_agents.back().mutate(EVOLUTION);
 
     }
 
     for(int i = num_best_to_keep + 1; i < NB_BRAIN; i+=1){
-        (*agents)[i] = new_agents[i]; 
+        (*brain_agents)[i] = new_agents[i]; 
 
     }
     
